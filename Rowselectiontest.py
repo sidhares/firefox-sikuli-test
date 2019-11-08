815f9ecaf2e191b49eece9d145d6fc316fa468ec
Hares Siddiqui <hares.siddiqui@ariztasoftware.com>
2019-11-08 22:18:46 +0530

    Made changes to test_home class and rowselection script

--- Rowselectiontest.py
+++ Rowselectiontest.py
@@ -9,33 +9,22 @@
 # * click different buttons in loop for 2 times
 # * click on VisitedPages and Bookmars 2 times in loop
 #-----------------------------------------------------------------------------
+from org.sikuli.script import Finder
 from org.hsid.auto.sikuli.iris.firefox import test_home
+
 firex = test_home()
 firex.init_test()
 firex.launchbrowser('https://github.com/')
 firex.perfromSikuliOperation("click()","image:####:OptionButton.png");
 firex.perfromSikuliOperation("click()","image:####:OptionImage.png");
-firex.sleep(2*500);
+firex.sleep(1*500);
 firex.perfromSikuliOperation("click()","image:####:Home.png");
 firex.sleep(1*500);
 
-#a little prep for the sorted function to get the y coord of the icon
-def byY(icon):
-    return icon.y
+firex.perfromSikuliOperation("findClick()", "image:####:1row.png:####:2") 
+firex.perfromSikuliOperation("click()", "image:####:2rows.png")
 
-#findAll() on your two identical icons and make them into a list
-bothIcons = list([x for x in findAll("1row.png")]) 
-#then sort them
-sortedIcons = sorted(bothIcons, key=byY)
-iconOnTop = sortedIcons[0]  
-iconOnBottom = sortedIcons[1]
-click(iconOnBottom);
-
-firex.perfromSikuliOperation("click()","image:####:2rows.png");
-firex.sleep(1*500);
-firex.perfromSikuliOperation("click()","image:####:2rows.png");
-firex.sleep(1*500);
-firex.perfromSikuliOperation("click()","image:####:Dropdown1row.png");
-firex.sleep(1*500);
+firex.perfromSikuliOperation("click()", "image:####:2rows.png") 
+firex.perfromSikuliOperation("click()", "image:####:Dropdown1row.png")
 
 firex.closebrowser()
